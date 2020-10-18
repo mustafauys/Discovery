fd57c3ea501d1485b27c70f0cb1cc1be9b1822a4
Mustafa <18701031@mersin.edu.tr>
2020-10-18 23:41:29 +0300

    Interface Studies Continue

--- lib/main.dart
+++ lib/main.dart
@@ -43,18 +43,104 @@ class _AnasayfaState extends State<Anasayfa> {
       body: ListView(
         padding: EdgeInsets.only(top: 10),
         children: [
+          // Üst taraftaki profil listesi
           Container(
-            color: Colors.blue.shade300,
-            height: 150,
+            //color: Colors.blue.shade300,
+            height: 140,
             width: double.infinity,
             child: ListView(
               padding: EdgeInsets.all(10),
               scrollDirection: Axis.horizontal,
               children: [
                 listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
               ],
             ),
-          )
+          ),
+
+          //Card
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              elevation: 4,
+              color: Colors.blue.shade300,
+              child: Container(
+                height: 450,
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: AssetImage('assets/model1.jpeg'),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width - 160,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Daisy",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                '34 mins ago',
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 12,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(
+                          Icons.more_vert,
+                          color: Colors.grey,
+                          size: 22,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -69,12 +155,45 @@ class _AnasayfaState extends State<Anasayfa> {
               height: 75,
               width: 75,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
+                borderRadius: BorderRadius.circular(38),
                 image: DecorationImage(
                     image: AssetImage(imagePath), fit: BoxFit.cover),
               ),
             ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                      image: AssetImage(logoPath), fit: BoxFit.contain),
+                ),
+              ),
+            ),
           ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 30,
+          width: 75,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.brown,
+          ),
+          child: Center(
+            child: Text(
+              "Follow",
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                color: Colors.white,
+              ),
+            ),
+          ),
         ),
       ],
     );
