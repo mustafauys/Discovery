3faacc1732b9a06639910f78e9d11a49fa2689e4
Mustafa <18701031@mersin.edu.tr>
2020-10-20 23:14:03 +0300

    Using InkWell and Hero Interface Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'detay.dart';
+
 void main() => runApp(ModaApp());
 
 class ModaApp extends StatelessWidget {
@@ -17,10 +19,62 @@ class Anasayfa extends StatefulWidget {
   _AnasayfaState createState() => _AnasayfaState();
 }
 
-class _AnasayfaState extends State<Anasayfa> {
+class _AnasayfaState extends State<Anasayfa>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          indicatorColor: Colors.transparent,
+          controller: tabController,
+          tabs: [
+            Tab(
+              icon: Icon(
+                Icons.more,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.play_arrow,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.navigation,
+                color: Colors.black,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.supervised_user_circle,
+                color: Colors.grey,
+                size: 22,
+              ),
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -148,117 +202,173 @@ class _AnasayfaState extends State<Anasayfa> {
                     SizedBox(height: 15),
                     Row(
                       children: [
-                        Container(
-                          height: 200,
-                          width: (MediaQuery.of(context).size.width - 50) / 2,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            image: DecorationImage(
-                                image: AssetImage('assets/modelgrid1.jpeg'),
-                                fit: BoxFit.cover),
+                        InkWell(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) =>
+                                    Detay(imgPath: 'assets/modelgrid1.jpeg')));
+                          },
+                          child: Hero(
+                            tag: 'assets/modelgrid1.jpeg',
+                            child: Container(
+                              height: 200,
+                              width:
+                                  (MediaQuery.of(context).size.width - 50) / 2,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                image: DecorationImage(
+                                    image: AssetImage('assets/modelgrid1.jpeg'),
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
                           ),
                         ),
                         SizedBox(width: 10),
                         Column(
                           children: [
-                            Container(
-                              height: 95,
-                              width:
-                                  (MediaQuery.of(context).size.width - 100) / 2,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                image: DecorationImage(
-                                    image: AssetImage('assets/modelgrid2.jpeg'),
-                                    fit: BoxFit.cover),
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => Detay(
+                                        imgPath: 'assets/modelgrid2.jpeg')));
+                              },
+                              child: Hero(
+                                tag: 'assets/modelgrid2.jpeg',
+                                child: Container(
+                                  height: 95,
+                                  width: (MediaQuery.of(context).size.width -
+                                          100) /
+                                      2,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/modelgrid2.jpeg'),
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
                               ),
                             ),
                             SizedBox(height: 10),
-                            Container(
-                              height: 95,
-                              width:
-                                  (MediaQuery.of(context).size.width - 100) / 2,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                image: DecorationImage(
-                                    image: AssetImage('assets/modelgrid3.jpeg'),
-                                    fit: BoxFit.cover),
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => Detay(
+                                        imgPath: 'assets/modelgrid3.jpeg')));
+                              },
+                              child: Hero(
+                                tag: 'assets/modelgrid3.jpeg',
+                                child: Container(
+                                  height: 95,
+                                  width: (MediaQuery.of(context).size.width -
+                                          100) /
+                                      2,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/modelgrid3.jpeg'),
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
                               ),
                             ),
                           ],
                         ),
-                        
                       ],
                     ),
                     SizedBox(height: 10),
-                        Row(
-                          children: [
-                            Container(
-                              height: 25,
-                              width: 100,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: Colors.brown.withOpacity(0.2),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "# Louis vuitton",
-                                  style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontSize: 10,
-                                    color: Colors.brown,
-                                  ),
-                                ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 25,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.brown.withOpacity(0.2),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "# Louis vuitton",
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 10,
+                                color: Colors.brown,
                               ),
                             ),
-                            SizedBox(width: 10),
-                            Container(
-                              height: 25,
-                              width: 75,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: Colors.brown.withOpacity(0.2),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "# Chloe",
-                                  style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontSize: 10,
-                                    color: Colors.brown,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
-                        SizedBox(height: 20),
-                        Divider(),
-                        SizedBox(height: 20),
-                        Row(
-                          children: [
-                            Icon(Icons.reply,color: Colors.brown.withOpacity(0.2), size: 30,),
-                            SizedBox(width: 10),
-                            Text("1.7k",style: TextStyle(fontFamily: 'Montserrat', fontSize: 16),),
-                            SizedBox(width: 25),
-                            Icon(Icons.comment,color: Colors.brown.withOpacity(0.2), size: 30,),
-                            SizedBox(width: 10),
-                            Text("325",style: TextStyle(fontFamily: 'Montserrat', fontSize: 16),),
-                            
-                            Container(
-                              width: MediaQuery.of(context).size.width-235,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Icon(Icons.favorite,color: Colors.red, size: 30,),
-                                  SizedBox(width: 5),
-                                  Text("2.3k",style: TextStyle(fontFamily: 'Montserrat', fontSize: 16),),
-                                  SizedBox(width: 25),
-                                ],
+                        SizedBox(width: 10),
+                        Container(
+                          height: 25,
+                          width: 75,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.brown.withOpacity(0.2),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "# Chloe",
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 10,
+                                color: Colors.brown,
                               ),
                             ),
-                            
-                          ],
+                          ),
                         ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Divider(),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.reply,
+                          color: Colors.brown.withOpacity(0.2),
+                          size: 30,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "1.7k",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                        SizedBox(width: 25),
+                        Icon(
+                          Icons.comment,
+                          color: Colors.brown.withOpacity(0.2),
+                          size: 30,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "325",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width - 235,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                                size: 30,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "2.3k",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat', fontSize: 16),
+                              ),
+                              SizedBox(width: 25),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
