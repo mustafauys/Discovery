0f55dd2faa83f2eb26512cb658d419c0c5e020d5
Mustafa <18701031@mersin.edu.tr>
2020-10-21 21:57:49 +0300

    Completing the Detail Page

--- lib/detay.dart
+++ lib/detay.dart
@@ -13,15 +13,108 @@ class _DetayState extends State<Detay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Hero(
-        tag: 'assets/modelgrid1.jpeg',
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
-        ),
+      body: Stack(
+        children: [
+          Hero(
+            tag: 'assets/modelgrid1.jpeg',
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 4,
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width - 30,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Container(
+                            height: 120,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: Colors.grey),
+                              image: DecorationImage(
+                                  image: AssetImage('assets/dress.jpg'),
+                                  fit: BoxFit.contain),
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "LAMINATED",
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              "Louis Vuitton",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(height: 10),
+                            Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width - 170,
+                              child: Text(
+                                "One button V-neck sling long-sleeved waist female stitching dress",
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Divider(),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, top: 10, bottom: 2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('\$6500', style: TextStyle(fontFamily: 'Montserrat', fontSize: 22),),
+                          Container(
+                            margin: EdgeInsets.only(right: 30),
+                            child: FloatingActionButton(
+                              onPressed: (){},
+                              child: Icon(Icons.arrow_forward_ios),
+                              backgroundColor: Colors.brown,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
